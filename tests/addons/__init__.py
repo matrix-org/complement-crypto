06ebf037eae049206f411733e07ac451ae1037b3
Kegan Dougal <7190048+kegsay@users.noreply.github.com>
2023-12-01 11:10:06 +0000

    More debug logging

--- tests/addons/__init__.py
+++ tests/addons/__init__.py
@@ -3,6 +3,7 @@ from mitmproxy.addons import asgiapp
 from status_code import StatusCode
 from controller import MITM_DOMAIN_NAME, app
 
+print("loading complement crypto addons")
 addons = [
     asgiapp.WSGIApp(app, MITM_DOMAIN_NAME, 80), # requests to this host will be routed to the flask app
     StatusCode(),
