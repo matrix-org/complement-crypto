a331cb0ffea7299db3731290fb666f3d3020122a
Kegan Dougal <7190048+kegsay@users.noreply.github.com>
2024-02-28 17:56:00 +0000

    convert remaining federation connectivity tests

--- tests/federation_connectivity_test.go
+++ tests/federation_connectivity_test.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/matrix-org/complement-crypto/internal/api"
-	"github.com/matrix-org/complement/helpers"
 	"github.com/matrix-org/complement/must"
 )
 
@@ -17,104 +16,74 @@ import (
 // B will be unable to decrypt C's message. TODO: see https://github.com/matrix-org/matrix-rust-sdk/issues/2864
 // Ensure sending another message from C is decryptable.
 func TestNewUserCannotGetKeysForOfflineServer(t *testing.T) {
-	deployment := Deploy(t)
-	csapiAlice := deployment.Register(t, "hs1", helpers.RegistrationOpts{
-		LocalpartSuffix: "alice",
-		Password:        "complement-crypto-password",
+	// normally we would use ForEachClient or ClientTypeMatrix but due to federation we need
+	// to pin it to certain langs/servers.
+	tc := CreateTestContext(t, api.ClientType{
+		Lang: api.ClientTypeRust,
+		HS:   "hs1",
+	}, api.ClientType{
+		Lang: api.ClientTypeJS,
+		HS:   "hs2",
+	}, api.ClientType{
+		Lang: api.ClientTypeRust,
+		HS:   "hs1",
 	})
-	csapiBob := deployment.Register(t, "hs2", helpers.RegistrationOpts{
-		LocalpartSuffix: "bob",
-		Password:        "complement-crypto-password",
+	roomID := tc.CreateNewEncryptedRoom(t, tc.Alice, "private_chat", []string{tc.Bob.UserID})
+	tc.Bob.MustJoinRoom(t, roomID, []string{"hs1"})
+
+	tc.WithAliceAndBobSyncing(t, func(alice, bob api.Client) {
+		// let clients sync device keys
+		time.Sleep(time.Second)
+
+		// ensure encrypted messaging works
+		wantMsgBody := "Hello world"
+		waiter := bob.WaitUntilEventInRoom(t, roomID, api.CheckEventHasBody(wantMsgBody))
+		evID := alice.SendMessage(t, roomID, wantMsgBody)
+		t.Logf("bob (%s) waiting for event %s", bob.Type(), evID)
+		waiter.Wait(t, 5*time.Second)
+
+		// now bob's HS becomes unreachable
+		tc.Deployment.PauseServer(t, "hs2")
+
+		// C now joins the room
+		tc.Alice.MustInviteRoom(t, roomID, tc.Charlie.UserID)
+		tc.WithClientSyncing(t, tc.CharlieClientType, tc.Charlie, func(charlie api.Client) {
+			tc.Charlie.MustJoinRoom(t, roomID, []string{"hs1"})
+
+			// let charlie sync device keys... and fail to get bob's keys!
+			time.Sleep(time.Second)
+
+			// send a message: bob won't be able to decrypt this, but alice will.
+			wantUndecryptableMsgBody := "Bob can't see this because his server is down"
+			waiter = alice.WaitUntilEventInRoom(t, roomID, api.CheckEventHasBody(wantUndecryptableMsgBody))
+			undecryptableEventID := charlie.SendMessage(t, roomID, wantUndecryptableMsgBody)
+			t.Logf("alice (%s) waiting for event %s", alice.Type(), undecryptableEventID)
+			waiter.Wait(t, 5*time.Second)
+
+			// now bob's server comes back online
+			tc.Deployment.UnpauseServer(t, "hs2")
+
+			// now we need to wait a bit for charlie's client to decide to hit /keys/claim again.
+			// If the client hits too often, there will be constantly send lag so long as bob's HS is offline.
+			// If the client hits too infrequently, there will be multiple undecryptable messages.
+			// See https://github.com/matrix-org/matrix-rust-sdk/issues/281 for why we want to backoff.
+			// See https://github.com/matrix-org/matrix-rust-sdk/issues/2804 for discussions on what the backoff should be.
+			t.Logf("sleeping until client timeout is ready...")
+			time.Sleep(20 * time.Second)
+
+			// send another message, bob should be able to decrypt it.
+			wantMsgBody = "Bob can see this because his server is now back online"
+			waiter = bob.WaitUntilEventInRoom(t, roomID, api.CheckEventHasBody(wantMsgBody))
+			evID = charlie.SendMessage(t, roomID, wantMsgBody)
+			t.Logf("bob (%s) waiting for event %s", bob.Type(), evID)
+			waiter.Wait(t, 5*time.Second)
+
+			// make sure bob cannot decrypt the msg from when his server was offline
+			// TODO: this isn't ideal, see https://github.com/matrix-org/matrix-rust-sdk/issues/2864
+			ev := bob.MustGetEvent(t, roomID, undecryptableEventID)
+			must.Equal(t, ev.FailedToDecrypt, true, "bob was able to decrypt the undecryptable event")
+		})
 	})
-	csapiCharlie := deployment.Register(t, "hs1", helpers.RegistrationOpts{
-		LocalpartSuffix: "charlie",
-		Password:        "complement-crypto-password",
-	})
-	roomID := csapiAlice.MustCreateRoom(t, map[string]interface{}{
-		"preset": "private_chat",
-		"invite": []string{csapiBob.UserID},
-		"initial_state": []map[string]interface{}{
-			{
-				"type":      "m.room.encryption",
-				"state_key": "",
-				"content": map[string]interface{}{
-					"algorithm": "m.megolm.v1.aes-sha2",
-				},
-			},
-		},
-	})
-
-	csapiBob.MustJoinRoom(t, roomID, []string{"hs1"})
-
-	ss := deployment.SlidingSyncURL(t)
-	alice := MustCreateClient(t,
-		api.ClientType{HS: "hs1", Lang: api.ClientTypeRust},
-		api.NewClientCreationOpts(csapiAlice), ss, WithDoLogin(t))
-	defer alice.Close(t)
-	bob := MustCreateClient(t,
-		api.ClientType{HS: "hs2", Lang: api.ClientTypeJS},
-		api.NewClientCreationOpts(csapiBob), ss, WithDoLogin(t))
-	defer bob.Close(t)
-	aliceStopSyncing := alice.MustStartSyncing(t)
-	defer aliceStopSyncing()
-	bobStopSyncing := bob.MustStartSyncing(t)
-	defer bobStopSyncing()
-
-	// let clients sync device keys
-	time.Sleep(time.Second)
-
-	// ensure encrypted messaging works
-	wantMsgBody := "Hello world"
-	waiter := bob.WaitUntilEventInRoom(t, roomID, api.CheckEventHasBody(wantMsgBody))
-	evID := alice.SendMessage(t, roomID, wantMsgBody)
-	t.Logf("bob (%s) waiting for event %s", bob.Type(), evID)
-	waiter.Wait(t, 5*time.Second)
-
-	// now bob's HS becomes unreachable
-	deployment.PauseServer(t, "hs2")
-
-	// C now joins the room
-	csapiAlice.MustInviteRoom(t, roomID, csapiCharlie.UserID)
-	charlie := MustCreateClient(t,
-		api.ClientType{HS: "hs1", Lang: api.ClientTypeRust},
-		api.NewClientCreationOpts(csapiCharlie), ss, WithDoLogin(t))
-	defer charlie.Close(t)
-	charlieStopSyncing := charlie.MustStartSyncing(t)
-	defer charlieStopSyncing()
-	csapiCharlie.MustJoinRoom(t, roomID, []string{"hs1"})
-
-	// let charlie sync device keys... and fail to get bob's keys!
-	time.Sleep(time.Second)
-
-	// send a message: bob won't be able to decrypt this, but alice will.
-	wantUndecryptableMsgBody := "Bob can't see this because his server is down"
-	waiter = alice.WaitUntilEventInRoom(t, roomID, api.CheckEventHasBody(wantUndecryptableMsgBody))
-	undecryptableEventID := charlie.SendMessage(t, roomID, wantUndecryptableMsgBody)
-	t.Logf("alice (%s) waiting for event %s", alice.Type(), undecryptableEventID)
-	waiter.Wait(t, 5*time.Second)
-
-	// now bob's server comes back online
-	deployment.UnpauseServer(t, "hs2")
-
-	// now we need to wait a bit for charlie's client to decide to hit /keys/claim again.
-	// If the client hits too often, there will be constantly send lag so long as bob's HS is offline.
-	// If the client hits too infrequently, there will be multiple undecryptable messages.
-	// See https://github.com/matrix-org/matrix-rust-sdk/issues/281 for why we want to backoff.
-	// See https://github.com/matrix-org/matrix-rust-sdk/issues/2804 for discussions on what the backoff should be.
-	t.Logf("sleeping until client timeout is ready...")
-	time.Sleep(20 * time.Second)
-
-	// send another message, bob should be able to decrypt it.
-	wantMsgBody = "Bob can see this because his server is now back online"
-	waiter = bob.WaitUntilEventInRoom(t, roomID, api.CheckEventHasBody(wantMsgBody))
-	evID = charlie.SendMessage(t, roomID, wantMsgBody)
-	t.Logf("bob (%s) waiting for event %s", bob.Type(), evID)
-	waiter.Wait(t, 5*time.Second)
-
-	// make sure bob cannot decrypt the msg from when his server was offline
-	// TODO: this isn't ideal, see https://github.com/matrix-org/matrix-rust-sdk/issues/2864
-	ev := bob.MustGetEvent(t, roomID, undecryptableEventID)
-	must.Equal(t, ev.FailedToDecrypt, true, "bob was able to decrypt the undecryptable event")
 }
 
 // A and B are in a room, on different servers.
