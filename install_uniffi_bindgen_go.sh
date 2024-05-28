492c1b0bf93332a1d84b359a8636fbd036ff5a2b
Kegan Dougal <7190048+kegsay@users.noreply.github.com>
2024-05-28 15:49:18 +0100

    Fix flakey TestRoomKeyIsCycledAfterEnoughTime
    Tests could fail with:
    ```
      thread 'tokio-runtime-worker' panicked at /home/runner/work/matrix-rust-sdk/matrix-rust-sdk/crates/matrix-sdk-crypto/src/session_manager/group_sessions.rs:211:9:
      Session expired
      note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    ```
    
    Due to this assertion being violated: `assert!(!session.expired(), "Session expired");`.
    
    My guess is that the code does something like:
     - check if expired session
     - if expired, then renew it
     - encrypt message, after first asserting that we are using a valid session
    
    This is unsafe, as it's effectively a read-modify-write race condition, since
    the session can expire due to time i.e the first check passes because it will
    expire in 1 nanosecond, then the assertion fails when encrypting.
    
    Also repoint `install_uniffi_bindgen_go.sh` to upstream.

--- install_uniffi_bindgen_go.sh
+++ install_uniffi_bindgen_go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 set -o pipefail
 
-git clone --depth 1 --branch main https://github.com/kegsay/uniffi-bindgen-go _temp_uniffi_bindgen_go;
+git clone --depth 1 --branch main https://github.com/NordSecurity/uniffi-bindgen-go _temp_uniffi_bindgen_go;
 (cd _temp_uniffi_bindgen_go && git submodule init && git submodule update && cargo install uniffi-bindgen-go --path ./bindgen);
 rm -rf _temp_uniffi_bindgen_go;
