4c4fc083fc60e2497958b329cd6261b6744a686a
Damir Jelić <poljar@termina.org.uk>
2024-12-06 13:42:15 +0100

    Simplify the install_uniffi_bindgen_go script
    There's no need to manually clone the uniffi-bindgen-go repo and then
    use the `cargo install` `--path` switch, we can use the `--git` option
    directly.
    
    This is also documented on the uniffi-bindgen-go
    repo: https://github.com/NordSecurity/uniffi-bindgen-go/?tab=readme-ov-file#how-to-install

--- install_uniffi_bindgen_go.sh
+++ install_uniffi_bindgen_go.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -e
 set -o pipefail
 
-git clone --depth 1 --branch main https://github.com/NordSecurity/uniffi-bindgen-go _temp_uniffi_bindgen_go;
-(cd _temp_uniffi_bindgen_go && git submodule init && git submodule update && cargo install uniffi-bindgen-go --path ./bindgen);
-rm -rf _temp_uniffi_bindgen_go;
+cargo install uniffi-bindgen-go --git https://github.com/NordSecurity/uniffi-bindgen-go
