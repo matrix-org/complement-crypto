ecd4ccb6592315344f8fc33c476c15f95adb60ad
Stefan Ceriu <stefan.ceriu@gmail.com>
2025-04-03 14:39:11 +0300

    Pin uniffi-bindgen-go to `v0.2.2+v0.25.0` bacause latest is failing the RustSDK - https://github.com/matrix-org/matrix-rust-sdk/actions/runs/14239463550/job/39906569970 - https://github.com/matrix-org/matrix-rust-sdk/actions/runs/14240730138/job/39910686891

--- install_uniffi_bindgen_go.sh
+++ install_uniffi_bindgen_go.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 set -o pipefail
 
-cargo install uniffi-bindgen-go --git https://github.com/NordSecurity/uniffi-bindgen-go
+cargo install uniffi-bindgen-go --tag v0.2.2+v0.25.0 --git https://github.com/NordSecurity/uniffi-bindgen-go
